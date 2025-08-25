bda95c4f5f8aad5c90d7d9f5a5ea49273bbd9d6a
Fabian Schuller <fabianschuller2005@gmail.com>
2025-08-26 01:15:03 +0200

    cleanup

--- r/src/gbm.R
+++ r/src/gbm.R
@@ -9,10 +9,8 @@ dax <- read_csv("data/dax.csv") %>%
   arrange(Date)
 
 log_returns <- diff(log(dax$Price))
-mu_daily <- mean(log_returns)
-sigma_daily <- sd(log_returns)
-mu_annual <- mu_daily * 252
-sigma_annual <- sigma_daily * sqrt(252)
+sigma <- sd(log_returns)
+mu <- mean(log_returns) + 0.5 * sigma^2
 
 set.seed(123)
 n <- 252
@@ -22,8 +20,8 @@ dt <- 1/n
 tgrid <- c(0, (1:n) * dt)
 
 simulations <- replicate(paths, {
-  W <- c(0, cumsum(rnorm(n, 0, sqrt(dt))))
-  S0 * exp((mu_annual - 0.5 * sigma_annual^2) * tgrid + sigma_annual * W)
+  W <- c(0, cumsum(rnorm(n, 0, 1)))
+  S0 * exp((mu - 0.5 * sigma^2) *  c(0, (1:n)) + sigma * W)
 })
 
 last_date <- max(dax$Date)
@@ -34,39 +32,40 @@ sim_df <- data.frame(
   Price = as.vector(simulations),
   Path = rep(1:paths, each = n+1)
 )
-sim_df_10 <- sim_df %>% filter(Path %in% 1:10)
+sim_df_examples <- sim_df %>% filter(Path %in% 1:4)
 
 ggplot() +
   geom_line(data = dax, aes(x = Date, y = Price), color = "black") +
-  geom_line(data = sim_df_10[-1, ], aes(x = Date, y = Price, group = Path, color = factor(Path)), alpha = 0.7) +
+  geom_line(data = sim_df_examples[-1, ], aes(x = Date, y = Price, group = Path, color = factor(Path)), alpha = 0.7) +
   theme_minimal() +
   guides(color = "none") +
   labs(title = "DAX Historical Prices and GBM Simulations", x = "Date", y = "Price")
 
 # Konfidenzintervalle
 
+alpha = 0.95#0.975
+beta = 1 - alpha
+
 # Simuliert
 sim_end <- simulations[n+1, ]
-ci <- quantile(sim_end, c(0.025, 0.975))
+ci <- quantile(sim_end, c(beta, alpha))
 ci
 
 # log-Normal Interval
-T <- 1
-z <- qnorm(c(0.025, 0.975))
-ci2 <- S0 * exp((mu_annual - 0.5 * sigma_annual^2) * T + z * sigma_annual * sqrt(T))
+T <- 252
+z <- qnorm(c(beta, alpha))
+ci2 <- S0 * exp((mu - 0.5 * sigma^2) * T + z * sigma * sqrt(T))
 ci2
 
 
 # log-Normal Interval täglich
 n <- 252
-dt <- 1/252
-tgrid <- (1:n) * dt
 last_date <- max(dax$Date)
 future_dates <- last_date + 1:n
 
-q_low <- S0 * exp((mu_annual - 0.5 * sigma_annual^2) * tgrid + qnorm(0.025) * sigma_annual * sqrt(tgrid))
-q_hi  <- S0 * exp((mu_annual - 0.5 * sigma_annual^2) * tgrid + qnorm(0.975) * sigma_annual * sqrt(tgrid))
-q_med <- S0 * exp((mu_annual - 0.5 * sigma_annual^2) * tgrid)
+q_low <- S0 * exp((mu - 0.5 * sigma^2) * (1:n) + qnorm(beta) * sigma * sqrt((1:n)))
+q_hi  <- S0 * exp((mu - 0.5 * sigma^2) * (1:n) + qnorm(alpha) * sigma * sqrt((1:n)))
+q_med <- S0 * exp((mu - 0.5 * sigma^2) * (1:n))
 
 band <- data.frame(Date = future_dates, low = q_low, mid = q_med, hi = q_hi)
 
